ca9d5619e1b2909ff7a39690893a4bce7dae8e15
Anastasiya-Shir <86313786+Anastasiya-Shir@users.noreply.github.com>
2022-08-10 22:41:22 +0300

    counter

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import { useDispatch, useSelector } from 'react-redux';
+import { decrementCount, incrementCount } from './store/actions';
+
 function App() {
+  const store = useSelector((store) => store);
+  const dispatch = useDispatch();
+  const counterValue = useSelector(state => {
+    return state.value;
+  });
+
+  const plus = () => {
+    dispatch(incrementCount())
+  };
+
+  const minus = () => {
+    dispatch(decrementCount())
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {counterValue}
+      <button onClick={plus}> plus</button>
+      <button onClick={minus}>minus</button>
     </div>
   );
 }
